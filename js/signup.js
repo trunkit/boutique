08df36b1c2b57c15cab4a7cd6807027e65a90bf9
William Weidendorf <wweidendorf@gmail.com>
2014-04-25 11:57:43 -0400

    Initial commit

---
+++ js/signup.js
@@ -0,0 +1,9 @@
+(function() {
+
+
+  $(document).ready(function(){
+  	$(".getinvited").mouseover(function(){
+   		$(".getInvited").fadeTo('fast',1); 
+  	})
+  })
+})();
---
+++ js/supply.js
@@ -0,0 +1,10 @@
+(function() {
+  var $getInvited = $('.js-get-invited');
+
+
+  $(document).ready(function(){
+  	$(".getinvited").mouseover(function(){
+   		$(".getInvited").fadeTo('fast',1); 
+  	})
+  })
+})();
